1f19a8f82dd5934a46eb90de6e4747ffa02f22fc
Feynman <codingmonkey83@gmail.com>
2021-02-23 14:16:08 +0800

    add S10gpio.sh

---
+++ board/freescale/imx6-rbc/rootfs_overlay/etc/init.d/S10gpio.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+IO_CFGS="488-out 489-out 490-out 491-out"
+IO_CFGS=$IO_CFGS" 492-in 493-in 494-in 495-in 496-in 497-in 498-in 499-in"
+IO_CFGS=$IO_CFGS" 500-in 501-in 502-in 503-in 504-in 505-in 506-in 507-in"
+IO_CFGS=$IO_CFGS" 508-out 509-out 510-out 511-out"
+
+GpioExport() {
+
+    for CFG in $IO_CFGS; do
+        IO_NUM=$(echo $CFG | cut -d"-" -f1)
+        IO_DIR=$(echo $CFG | cut -d"-" -f2)
+        echo $IO_NUM >/sys/class/gpio/export
+        echo $IO_DIR >/sys/class/gpio/gpio${IO_NUM}/direction
+    done
+
+}
+
+GpioUnExport() {
+
+    for CFG in $IO_CFGS; do
+        IO_NUM=$(echo $CFG | cut -d"-" -f1)
+        IO_DIR=$(echo $CFG | cut -d"-" -f2)
+        echo $IO_NUM >/sys/class/gpio/unexport
+        # echo $IO_DIR >/sys/class/gpio/gpio${IO_NUM}/direction
+    done
+
+}
+
+case "$1" in
+start)
+    printf "Starting export gpio"
+    GpioExport
+    ;;
+stop)
+    printf "Stopping export gpio"
+    GpioUnExport
+    ;;
+restart)
+    printf "Restarting export gpio"
+    GpioUnExport
+    GpioExport
+    ;;
+*)
+    echo "Usage: /etc/init.d/gpio.sh {start|stop|restart}"
+    exit 1
+    ;;
+esac
+
+exit 0
