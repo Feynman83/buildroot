657312b5a52e2da4a3d0c4e45eb54846af96a7f4
Feynman <codingmonkey83@gmail.com>
2021-02-24 14:22:55 +0800

    add soft link

--- board/freescale/imx6-rbc/rootfs_overlay/etc/init.d/S10gpio.sh
+++ board/freescale/imx6-rbc/rootfs_overlay/etc/init.d/S10gpio.sh
@@ -1,26 +1,29 @@
 #!/bin/sh
 
-IO_CFGS="488-out 489-out 490-out 491-out"
-IO_CFGS=$IO_CFGS" 492-in 493-in 494-in 495-in 496-in 497-in 498-in 499-in"
-IO_CFGS=$IO_CFGS" 500-in 501-in 502-in 503-in 504-in 505-in 506-in 507-in"
-IO_CFGS=$IO_CFGS" 508-out 509-out 510-out 511-out"
+IO_CFGS="488-out-do1 489-out-do2 490-out-do3 491-out-do4"
+IO_CFGS=$IO_CFGS" 492-in-di1 493-in-di2 494-in-di3 495-in-di4 496-in-di5 497-in-di6 498-in-di7 499-in-di8"
+IO_CFGS=$IO_CFGS" 500-in-di9 501-in-di10 502-in-di11 503-in-di12 504-in-di13 505-in-di14 506-in-di15 507-in-di16"
+IO_CFGS=$IO_CFGS" 508-out-uart1_sw 509-out-uart2_sw 510-out-uart3_sw 511-out-uart4_sw"
 # AI switch gpio
-IO_CFGS=$IO_CFGS" 135-out 136-out 192-out 196-out 127-out 133-out 134-out 197-out"
-IO_CFGS=$IO_CFGS" 1-out 200-out 5-out 194-out 115-out 113-out 112-out 114-out"
-IO_CFGS=$IO_CFGS" 21-out 111-out 109-out 107-out 106-out 108-out 110-out 17-out"
+IO_CFGS=$IO_CFGS" 135-out-av1_sw 136-out-av2_sw 192-out-av3_sw 196-out-av4_sw 127-out-av5_sw 133-out-av6_sw 134-out-av7_sw 197-out-av8_sw"
+IO_CFGS=$IO_CFGS" 1-out-av9_sw 200-out-av10_sw 5-out-av11_sw 194-out-av12_sw 115-out-av13_sw 113-out-av14_sw 112-out-av15_sw 114-out-av16_sw"
+IO_CFGS=$IO_CFGS" 21-out-av17_sw 111-out-av18_sw 109-out-av19_sw 107-out-av20_sw 106-out-av21_sw 108-out-av22_sw 110-out-av23_sw 17-out-av24_sw"
 GpioExport() {
 
+    mkdir -p /dev/gpio
     for CFG in $IO_CFGS; do
         IO_NUM=$(echo $CFG | cut -d"-" -f1)
         IO_DIR=$(echo $CFG | cut -d"-" -f2)
+        IO_ALIAS=$(echo $CFG | cut -d"-" -f3)
         echo $IO_NUM >/sys/class/gpio/export
         echo $IO_DIR >/sys/class/gpio/gpio${IO_NUM}/direction
+        ln -s /sys/class/gpio/gpio${IO_NUM} /dev/gpio/${IO_ALIAS}
     done
 
 }
 
 GpioUnExport() {
-
+    rm -rf /dev/gpio
     for CFG in $IO_CFGS; do
         IO_NUM=$(echo $CFG | cut -d"-" -f1)
         IO_DIR=$(echo $CFG | cut -d"-" -f2)
