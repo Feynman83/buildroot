6ce238c3d3e417d0175450403013aa65af101c0c
Feynman <codingmonkey83@gmail.com>
2021-03-01 17:09:58 +0800

    change 232 switch gpio

--- board/freescale/imx6-rbc/rootfs_overlay/etc/init.d/S10gpio.sh
+++ board/freescale/imx6-rbc/rootfs_overlay/etc/init.d/S10gpio.sh
@@ -4,7 +4,7 @@ IO_CFGS="488-out-do1 489-out-do2 490-out-do3 491-out-do4"
 IO_CFGS=$IO_CFGS" 492-in-di1 493-in-di2 494-in-di3 495-in-di4 496-in-di5 497-in-di6 498-in-di7 499-in-di8"
 # IO_CFGS=$IO_CFGS" 500-in-di9 501-in-di10 502-in-di11 503-in-di12 504-in-di13 505-in-di14 506-in-di15 507-in-di16"
 IO_CFGS=$IO_CFGS" 503-in-di9 507-in-di10 502-in-di11 501-in-di12 506-in-di13 500-in-di14 505-in-di15 504-in-di16"
-IO_CFGS=$IO_CFGS" 508-out-uart1_sw 509-out-uart2_sw 510-out-uart3_sw 511-out-uart4_sw"
+IO_CFGS=$IO_CFGS" 511-out-uart1_sw 509-out-uart2_sw 508-out-uart3_sw 510-out-uart4_sw"
 # AI switch gpio
 IO_CFGS=$IO_CFGS" 135-out-av1_sw 136-out-av2_sw 192-out-av3_sw 196-out-av4_sw 127-out-av5_sw 133-out-av6_sw 134-out-av7_sw 197-out-av8_sw"
 IO_CFGS=$IO_CFGS" 1-out-av9_sw 200-out-av10_sw 5-out-av11_sw 194-out-av12_sw 115-out-av13_sw 113-out-av14_sw 112-out-av15_sw 114-out-av16_sw"
