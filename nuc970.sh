de88d46002fc852736d76e17bd7694e0da792bb3
Feynman <codingmonkey83@gmail.com>
2021-05-27 13:52:45 +0800

    add rcS

--- nuc970.sh
+++ nuc970.sh
@@ -3,3 +3,12 @@
 chmod 600 ${TARGET_DIR}/etc/ssh/ssh_host*
 chmod 600 ${TARGET_DIR}/etc/profile
 chmod 600 ${TARGET_DIR}/etc/hostname
+
+
+
+export DATA_UBIFS_FILE=${BASE_DIR}/images/data.ubifs
+mkdir -p /tmp/nuc970-data
+mkfs.ubifs -F -x lzo -m 2KiB -e 124KiB -c 560 -o ${DATA_UBIFS_FILE} -d /tmp/nuc970-data
+
+sed -e 's@BR2_DATA_UBI_PATH@'"$DATA_UBIFS_FILE"'@' ${BASE_DIR}/../fs/ubi/ubinize_data.cfg >> ${BASE_DIR}/images/ubinize_data.cfg
+ubinize -o ${BASE_DIR}/images/data.ubi -m 2KiB -p 128KiB  -s 2048 ${BASE_DIR}/images/ubinize_data.cfg
