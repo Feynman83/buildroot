c105046b0c6bd203671c1d53babac73247c8f337
Feynman <codingmonkey83@gmail.com>
2021-05-31 15:01:03 +0800

    fix auto detect

--- board/arm/nuc970/etc/init.d/S11startup.sh
+++ board/arm/nuc970/etc/init.d/S11startup.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
 
 if [ -c /dev/fb0 ]; then
-    echo "XBM" >/etc/hostname\
+    echo "XBM" >/etc/hostname
     rm -rf /boot/zImage-A02
     rm -rf /boot/zImage-C02
+    if [ -f /boot/970uimage-XBM ]; then
+        flash_erase /dev/mtd1 0 0
+        nandwrite -p /dev/mtd1 /boot/970uimage-XBM
+        rm -rf /boot/zImage-XBM
+    fi
+   
 elif [ -c /dev/i2c-0 ]; then
     echo "RBC02" >/etc/hostname
     rm -rf /boot/zImage-XBM
     rm -rf /boot/zImage-A02
+    if [ -f /boot/970uimage-C02 ]; then
+        flash_erase /dev/mtd1 0 0
+        nandwrite -p /dev/mtd1 /boot/970uimage-C02
+        rm -rf /boot/zImage-C02
+    fi
 else
     echo "XDUD-G3000" >/etc/hostname
     rm -rf /boot/zImage-XBM
     rm -rf /boot/zImage-C02
+    if [ -f /boot/970uimage-A02 ]; then
+        flash_erase /dev/mtd1 0 0
+        nandwrite -p /dev/mtd1 /boot/970uimage-A02
+        rm -rf /boot/zImage-A02
+    fi
 fi
 
-umount /home/xbrother
+rm -rf /boot/970uimage*
+ln -s /home/xbrother /xbrother
+
+umount /home
 sleep 1
 ubidetach -m 3
 sleep 1
@@ -27,12 +46,16 @@ sleep 1
 if [ -c /dev/ubi1 ]; then
     ubimkvol /dev/ubi1 -m -N data
     sleep 1
-    mount -t ubifs ubi1:data /home/xbrother
+    mount -t ubifs ubi1:data /home
 else
     ubimkvol /dev/ubi0 -m -N data
     sleep 1
-    mount -t ubifs ubi0:data /home/xbrother
+    mount -t ubifs ubi0:data /home
 fi
+
+mkdir -p /home/xbrother/app
+mkdir -p /home/xbrother/data
+mkdir -p /home/legion
 
 rm -rf /etc/init.d/S11startup.sh
 
